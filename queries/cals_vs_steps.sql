8e7282e0a041c91a11cae25d58e268f27a554646
Alyssa McCormick <156459098+mccormickal@users.noreply.github.com>
2025-03-06 11:38:15 -0500

    Delete queries/cals_vs_steps.sql

--- queries/cals_vs_steps.sql
+++
@@ -1,5 +0,0 @@
-/* Identify the correlation between step count and calories burned */
-SELECT
-  ROUND(CORR(steps.StepTotal, cals.Calories), 2) AS Steps_Calories_Correlation
-FROM `bellabeat-case-study-analysis.FitBit_Fitness_Tracker_Dataset.dailySteps` steps
-JOIN `bellabeat-case-study-analysis.FitBit_Fitness_Tracker_Dataset.dailyCalories` cals USING (Id, ActivityDay);
