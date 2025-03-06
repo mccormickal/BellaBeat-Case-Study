e5f98e78fdbc9c606f6872fb5517be7d58d769b5
Alyssa McCormick <156459098+mccormickal@users.noreply.github.com>
2025-03-06 11:36:44 -0500

    Update avg_segment_minutes.sql

--- queries/avg_segment_minutes.sql
+++ queries/avg_segment_minutes.sql
@@ -1,7 +1,7 @@
 /* Group user based on activity levels */
 SELECT
   ActivitySegment,
-  SUM(AvgActiveMinutes)
+  ROUND(AVG(AvgActiveMinutes), 2) AS AvgMinutes
 FROM `bellabeat-case-study-analysis.FitBit_Fitness_Tracker_Dataset.userActivityLevelsView`
 GROUP BY ActivitySegment
-ORDER BY SUM(AvgActiveMinutes) DESC;
+ORDER BY AvgMinutes DESC;
